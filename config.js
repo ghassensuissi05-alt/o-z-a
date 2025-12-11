fb6191cd0f45a870c6dd488dcbd9e81cf70d5717
ghassensuissi05-alt <ghassensuissi05@gmail.com>
2025-12-11 09:42:00 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// ملف منفصل لحماية التوكن
+const GITHUB_TOKEN = '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';
+const REPO_OWNER = 'ghassensuissi05-alt';
+const REPO_NAME = 'o-z-a';
+const FILE_PATH = 'users.json';
